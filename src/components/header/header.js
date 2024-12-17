70f6f86a3f420b70896685abebe3cf43e17abfaa
Ashfi <ashfimohamed@gmail.com>
2024-12-17 19:14:13 +0500

    header changed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,30 @@
 import React from "react"
 import styles from "./headercss.module.css"
 import { Link } from 'react-router-dom';
+import Logo from "/Users/ashfi/Desktop/react/react-components/src/assets/header1logo.jpg"
+import Background from "/Users/ashfi/Desktop/react/react-components/src/assets/header1background.jpg"
 
 const Header = () => {
     return (
         <div className={styles.container}>
-            <Link to="/page1">page1</Link>
-            <Link to="/page2">page2</Link>
-            <Link to="/page3">page3</Link>
+
+            <div className={styles.logobox}>
+                
+            </div>
+            <div className={styles.linkbox}>
+                <Link to="/page1" className={styles.link}>page1</Link>
+                <Link to="/page2" className={styles.link}>page2 </Link>
+                <Link to="/page3" className={styles.link}>page3</Link>
+                
+            </div>
+            <div className={styles.buttonbox}>
+                <button className={styles.button}>login</button>
+                <button className={styles.button}>signup</button>
+
+            </div>
+ 
+            
+           
         </div>
     )
 }
