5e3b0b4a7e67a7e11195b9c83cf81e193ac428b2
Ashfi <ashfimohamed@gmail.com>
2024-12-28 14:30:34 +0500

    fetch from API

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -3,11 +3,13 @@ import styles from "../pages/page1.module.css"
 import FormComp from "../components/forms/form"
 import ListComp from "../components/listcomp/listcomp"
 import { useUser } from "../context/usercontext"
+import CallAPI from "../services/apicall"
 
 
 const Page1 = () => {
-
     const {user} = useUser();
+
+    const url = "https://randomuser.me/api/"
     return (
         <div className={styles.page1}>
             page 1
@@ -15,6 +17,7 @@ const Page1 = () => {
                 <h2>{user.name}</h2>
                 <ListComp/>
                 <FormComp/>
+                <CallAPI url = {url}/>
 
             </div>
         </div>
---
+++ src/services/apicall.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState} from "react";
+
+
+const CallAPI = (props) => {
+
+    const [data, setData] = useState({});
+    const url = props.url;
+
+    const fetchData = (url) => {
+        fetch(url)
+        .then((response) => response.json())
+        .then((jsonData) => setData(jsonData.results))
+        .catch((error) => console.log(error));
+    }
+
+    useEffect(() => {
+        fetchData(url);
+    }, [url]);
+
+    return(Object.keys(data).length > 0 ? (<div>
+        call API
+        <div>
+            {data.map((object) => {
+                const firstName = object.name.first
+                const lastName = object.name.last
+                const title = object.name.title
+                return (<div>
+                    <h3>first name: {firstName}</h3>
+                    <h3>last name: {lastName}</h3>
+                    <h3>title: {title}</h3>
+                </div>)
+            })}
+        </div>
+
+
+    </div>) : (
+        <div>
+            data is pending
+        </div>
+    )
+        
+    )
+}
+
+export default CallAPI;
